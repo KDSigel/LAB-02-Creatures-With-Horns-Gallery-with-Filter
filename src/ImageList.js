d1e23264390be23d61d9c749cfe9fe474bed6c86
Karl Sigel <heykarl@gmail.com>
2021-09-21 13:28:54 -0700

    built out basic struture? Maybe? super confused

---
+++ src/ImageList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './data.js'
+
+export default class ImageList extends Component {
+
+
+
+
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
